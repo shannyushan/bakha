6a132cea3d7a15904ff1f1e1f22e25d3a7637e86
shannyushan <nyushan.darshan@gmail.com>
2022-04-01 22:21:39 +0545

    Navigation:isauthed condition

--- components/Navigation/index.tsx
+++ components/Navigation/index.tsx
@@ -1,24 +1,56 @@
 import { Box, Flex, HStack } from "@chakra-ui/layout";
+import { Button, Link } from "@chakra-ui/react";
 import next from "next";
 import Image from "next/image";
-import Link from "next/link";
+import NextLink from "next/link";
 import logosrc from "./../../public/Bakha.png";
 
 import styles from "./Navigation.module.css";
 
-const Navigation = () => {
+const Navigation = ({ isauthed, activePage }) => {
   return (
     <nav className={styles.nav}>
       <HStack alignItems={"center"} className={styles.Navigation}>
-        <Box>
+        <Box marginLeft={"2em"}>
           <Image src={logosrc} alt="Bakha Logo" width="40px" height="60px" />
         </Box>
         <HStack as={"ul"} className="nav-menu" alignItems="center">
-          <Link href={"/"}>Home</Link>
-          <Link href={"/"}>Explore</Link>
-          <Link href={"/"}>Bakha</Link>
-          <Link href={"/"}>About</Link>
-          <Link href={"/"}>Contact</Link>
+          
+          <NextLink href="/" passHref>
+            <Link>Home</Link>
+          </NextLink>
+          <NextLink href="/" passHref>
+            <Link>Explore</Link>
+          </NextLink>
+          {isauthed ? (
+            <>
+              <NextLink href="/" passHref>
+                <Link>My Stories</Link>
+              </NextLink>
+              <NextLink href="/" passHref>
+                <Link>Profile</Link>
+              </NextLink>
+            </>
+          ) : (
+            <>
+              <NextLink href="/" passHref>
+                <Link>About</Link>
+              </NextLink>
+              <NextLink href="/" passHref>
+                <Link>Contact</Link>
+              </NextLink>
+            </>
+          )}
+        </HStack>
+        <HStack alignItems={"center"}>
+          {isauthed ? (
+            <Button>Logout</Button>
+          ) : (
+            <>
+              <Button>Register</Button>
+              <Button>Login</Button>
+            </>
+          )}
         </HStack>
       </HStack>
     </nav>
