4e6221036d66dba9ab3fee6212fcd63b9f8c394b
shannyushan <nyushan.darshan@gmail.com>
2022-04-01 18:26:31 +0545

    chakraui basic provider + typescript

---
+++ pages/index.tsx
@@ -0,0 +1,20 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+
+const Home: NextPage = () => {
+  return (
+    <div className="">
+     <Head>
+       <title>Bakha: </title>
+     </Head>
+     <div>
+     <header></header>
+     <main> Hello </main>
+   </div>
+    </div>
+  )
+}
+
+export default Home
---
+++ pages/theme.js
@@ -0,0 +1,23 @@
+import { extendTheme } from '@chakra-ui/react'
+
+// 2. Extend the theme to include custom colors, fonts, etc
+const colors = {
+  brand: {
+    900: '#1a365d',
+    800: '#153e75',
+    700: '#2a69ac',
+  },
+}
+
+
+// 2. Add your color mode config
+const config= {
+  initialColorMode: 'light',
+  useSystemColorMode: false,
+}
+
+
+const theme = extendTheme({ colors })
+
+
+export default theme
